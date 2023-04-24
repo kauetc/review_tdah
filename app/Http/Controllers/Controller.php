5190963fcd1bebbdae4db6e50cefeb723f5a1c5f
Kaue Trevisan Correa <kaue.correa@vmaxsys.com.br>
2023-04-24 20:11:00 -0300

    commit inicial com a estrutura basica do projeto

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+    public function index(Request $request){
+        dd($request);
+    }
+}
