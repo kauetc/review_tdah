2c6cf2a092b9f35ac72586e9c555521d03d99478
Beatriz <b.rpaschoalini@gmail.com>
2023-05-02 18:57:10 -0300

    alteracoes pagina de register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Review TDAH</title>
+        <title>Cadastro - Review TDAH</title>
 
     
     </head>
