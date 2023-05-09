45da5d13239568517f4f9306d699518b1a7ad39f
Kaue Trevisan Correa <kaue.correa@vmaxsys.com.br>
2023-05-08 21:58:05 -0300

    exemplo handling errors

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -31,6 +31,9 @@
                     <div class="column-1">
                         <label for="InputNome">Nome</label>
                         <input type="text" class="form-control" id="InputNome" name="nome" placeholder="José da Silva"/>       
+                        @if ($errors->has('nome'))
+                            <p class="">{{ $errors->first('nome') }}</p>
+                        @endif
                     </div>
                     </div>
 
@@ -93,33 +96,6 @@
                 <div class="form-group">
                     <label for="uf">Estado</label>
                         <select id="uf" name="uf">
-                            <option value="AC">Acre</option>
-                            <option value="AL">Alagoas</option>
-                            <option value="AP">Amapá</option>
-                            <option value="AM">Amazonas</option>
-                            <option value="BA">Bahia</option>
-                            <option value="CE">Ceará</option>
-                            <option value="DF">Distrito Federal</option>
-                            <option value="ES">Espírito Santo</option>
-                            <option value="GO">Goiás</option>
-                            <option value="MA">Maranhão</option>
-                            <option value="MT">Mato Grosso</option>
-                            <option value="MS">Mato Grosso do Sul</option>
-                            <option value="MG">Minas Gerais</option>
-                            <option value="PA">Pará</option>
-                            <option value="PB">Paraíba</option>
-                            <option value="PR">Paraná</option>
-                            <option value="PE">Pernambuco</option>
-                            <option value="PI">Piauí</option>
-                            <option value="RJ">Rio de Janeiro</option>
-                            <option value="RN">Rio Grande do Norte</option>
-                            <option value="RS">Rio Grande do Sul</option>
-                            <option value="RO">Rondônia</option>
-                            <option value="RR">Roraima</option>
-                            <option value="SC">Santa Catarina</option>
-                            <option value="SP">São Paulo</option>
-                            <option value="SE">Sergipe</option>
-                            <option value="TO">Tocantins</option>
                         </select>
                 </div>
 
@@ -161,7 +137,6 @@
                         console.log('aoba');	
 						var options = '';
                         $.each(resposta, function(index, value) {
-                            console.log(index, value);
                             var isSelected = (value.sigla === "{{ old('uf') }}") ? 'selected' : '';
                             options += '<option value="' + value.sigla + '" ' + isSelected + '>' + value.nome + '</option>';
                         });
