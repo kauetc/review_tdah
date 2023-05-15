6f71e46721e41285a6763a754eb12541b5cd7228
Kaue Trevisan Correa <kaue.correa@vmaxsys.com.br>
2023-05-15 20:06:11 -0300

    dashboard page

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title> Teste login</title>
+        <link rel="stylesheet" href="login.css"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Jost:wght@300&display=swap" rel="stylesheet">
+    </head>
+
+    <body>                   
+       <div class="center-square">
+            <h1>TESTE</h1>          
+       </div>     
+    </body>
+
+</html>
