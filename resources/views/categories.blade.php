3043c3003e4bcaf92e6be5a40910e7280ae8a748
Beatriz <b.rpaschoalini@gmail.com>
2023-05-22 21:32:04 -0300

    desenvolvimento da página de categorias

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,20 +6,59 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Categorias</title>        
         <link rel="stylesheet" type="text/css" href="{{ asset('css/dashboard.css') }}"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@200&display=swap" rel="stylesheet">
     </head>
 
     <body>
-        <div id="page-header">
-            <h1>Categorias</h1>
+        <div class="page-header">
+            <header>
+                <h1>Categorias</h1>
+                <a class="top-bar-settings" href="{{route('settings');}}">Configurações</a>
+                <!-- Hiperlink sair? (canto superior direito)-->
+            </header>
         </div>
+        
         <div id="content">
+            <div class="div-header">
+                <div id="div-search" class="div-search">
+                    <input type="text" id="searchbar" placeholder="Procurar categoria..."/>
+                    <button id="button-search"> Pesquisar</button>
+                </div>
+
+                <div id="div-newcategory" class="div-newcategory">
+                    <button id="button-newcategory"> Adicionar categoria</button>
+                </div>
+            </div>
+
+            <div id="table">
+                <table>
+                    <thead>
+                        <tr>
+                            <td>Nome da categoria</td>
+                            <td>Created at</td>
+                            <td>Updated at</td>
+                            <td>Alteração por usuário</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <button id="button-edit-category">Editar</button>
+
+
+
+
+
+
             {{-- 
-                Cabeçalho com informação da pagina
+                XXCabeçalho com informação da paginaXX
                 botoes de adicionar editar deletar
                 tabela com registros
                 menu
                 footer
             --}}
+
+
             
             
             @foreach($categories as $category)
@@ -29,10 +68,35 @@
                             <img src="https://nox.com.br/wp-content/uploads/2022/12/como-montar-uma-lanchonete-nox-automacao-blog-scaled-e1670341533976-1024x684.jpg"  width="100%" height="100%"/> 
                         </div>
                         <h1>{{$category->nome_categoria}}</h1> 
+
                     </div>
                 </a>
             @endforeach
                     
         </div>
+
+        
+        
+
+        <!-- É isso mesmo?
+        <button onclick="adicionarDiv()">Adicionar Div</button> 
+        <script>
+            function adicionarDiv() {
+            var novaDiv = document.createElement("div");
+            novaDiv.className = "column";
+            novaDiv.innerHTML = "Nova Div";
+
+            var colunas = document.querySelector(".row");
+            colunas.appendChild(novaDiv);
+            }
+        </script>
+        -->
+
+        <div class="page-footer">
+            <footer>
+
+            </footer>
+        </div>
+
     </body>
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,10 +27,8 @@
 
                 <a href="https://www.google.com.br/" target=”_blank”>
                     <div class="column" id="restaurante">
-                        <div class="image">
-                            
+                        <div class="image">                            
                             <img src="https://a.cdn-hotels.com/gdcs/production156/d1067/98b5cb4d-ea36-43fd-b3ca-21ae04c7c770.jpg" width="100%" height="100%"/>
-                            
                         </div>
                         <h1 id="rest">RESTAURANTES</h1>                  
                     </div>
@@ -47,12 +45,9 @@
 
                 <a href="https://www.google.com.br/" target=”_blank”>
                     <div class="column" >
-                    <div class="image">
-                        
-                        <img src="https://f.i.uol.com.br/fotografia/2018/07/26/15326332335b5a209143c97_1532633233_3x2_md.jpg" width="100%" height="100%"/> 
-                        
-                    </div>
-                    
+                    <div class="image">                       
+                        <img src="https://f.i.uol.com.br/fotografia/2018/07/26/15326332335b5a209143c97_1532633233_3x2_md.jpg" width="100%" height="100%"/>                        
+                    </div>                    
                     <h1>BARES</h1>
                     
                                    
@@ -63,11 +58,8 @@
                     <div class="column">
                         <div class="image">
                             <img src="https://www.farofamagazine.com.br/wp-content/uploads/2022/05/unnamed-12.jpg" width="100%" height="100%"/>  
-                        </div>
-                        
-                        <h1>PADARIAS</h1>
-                        
-                                        
+                        </div>                        
+                        <h1>PADARIAS</h1>                                                                
                     </div>
                 </a>   
             </div>      
@@ -77,12 +69,12 @@
             <div id="expand-container">
                 <div id="expand-contract" class="expanded">
                     <table>
-                        <tr><td><a href="{{route('categories');}}">Categorias</td></tr>
-                        <tr><td><a href="{{route('subcategories');}}">Subcategorias</td></tr>
-                        <tr><td><a href="{{route('reviews');}}">Reviews</td></tr>
+                        <tr><td><a href="{{route('categories');}}">Categorias</a></td></tr>
+                        <tr><td><a href="{{route('subcategories');}}">Subcategorias</a></td></tr>
+                        <tr><td><a href="{{route('reviews');}}">Reviews</a></td></tr>
                         <tr><td>Análise dos dados</td></tr>
-                        <tr><td><a href="{{route('settings');}}">Configurações</td></tr>
-                        <tr><td><a href="{{route('profile');}}">Perfil</td></tr>
+                        <tr><td><a href="{{route('settings');}}">Configurações</a></td></tr>
+                        <tr><td><a href="{{route('profile');}}">Perfil</a></td></tr>
                     </table>
                 </div>
             </div>
